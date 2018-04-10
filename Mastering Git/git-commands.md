345ca0c7413b72af2c5481a996937fca82b8c295
Tanner Barlow <tanner.barlow12@gmail.com>
2018-04-10 16:25:57 -0700

    Finished example of multipart form data request

--- Mastering Git/git-commands.md
+++ Mastering Git/git-commands.md
@@ -10,5 +10,31 @@
     - removes file from index, but keeps it in working area
 - git commit
     - sends files from index to repository
-- 
+
+
+Moving and Renaming
+- Option 1
+    - git mv <old filename> <new filename>
+    - git add <new filename>
+    - git add <old filename>
+    - The old filename will show as deleted in the git status and the new file has been added
+    - By doing a 'git add' we update the index with the appropriate file content
+
+
+Git Reset
+- reset does different things in different contexts
+- git reset <commit> 
+    - moves the branch (and HEAD along with it) to point to that specific commit
+- git reset --hard
+    - copies files from repository to BOTH working area and index, overwriting any changes
+- git reset --mixed (default option)
+    - copies file from repo to JUST the index, overwriting any changes, but leaving the working area alone
+- git reset --soft
+    - Don't do anything to working area and index
+
+Commands that move branches
+- commit
+- merge
+- rebase
+- pull
 
---
+++ MultipartFormRequest/MultipartFormRequest/Program.cs
@@ -0,0 +1,220 @@
+﻿using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultipartFormRequest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var result = ExecutePostRequest(
+                new Uri("https://fool-the-ai-api.azurewebsites.net/api/Image"),
+                new Dictionary<string, string>
+                {
+                    {"tag", "basketball" },
+                    {"username","tanner" }
+                },
+                new FileInfo(@"C:\Users\tabarlow\Pictures\nba-basketball-wallpaper-843.jpg"),
+                "image/jpeg",
+                "file"
+            );
+            Console.WriteLine(result);
+            Console.ReadLine();
+
+        }
+
+        public static string ExecutePostRequest(
+              Uri url,
+              Dictionary<string, string> postData,
+              FileInfo fileToUpload,
+              string fileMimeType,
+              string fileFormKey
+            )
+        {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url.AbsoluteUri);
+            request.Method = "POST";
+            request.KeepAlive = true;
+            string boundary = CreateFormDataBoundary();
+            request.ContentType = "multipart/form-data; boundary=" + boundary;
+            Stream requestStream = request.GetRequestStream();
+            postData.WriteMultipartFormData(requestStream, boundary);
+            if (fileToUpload != null)
+            {
+                fileToUpload.WriteMultipartFormData(requestStream, boundary, fileMimeType, fileFormKey);
+            }
+            byte[] endBytes = System.Text.Encoding.UTF8.GetBytes("--" + boundary + "--");
+            requestStream.Write(endBytes, 0, endBytes.Length);
+            requestStream.Close();
+            using (WebResponse response = request.GetResponse())
+            using (StreamReader reader = new StreamReader(response.GetResponseStream()))
+            {
+                return reader.ReadToEnd();
+            };
+        }
+
+        /// <summary>
+        /// Creates a multipart/form-data boundary.
+        /// </summary>
+        /// <returns>
+        /// A dynamically generated form boundary for use in posting multipart/form-data requests.
+        /// </returns>
+        private static string CreateFormDataBoundary()
+        {
+            return "---------------------------" + DateTime.Now.Ticks.ToString("x");
+        }
+    }
+
+    /// <summary>
+    /// Extension methods for generic dictionaries.
+    /// </summary>
+    public static class DictionaryExtensions
+    {
+        /// <summary>
+        /// Template for a multipart/form-data item.
+        /// </summary>
+        public const string FormDataTemplate = "--{0}\r\nContent-Disposition: form-data; name=\"{1}\"\r\n\r\n{2}\r\n";
+
+        /// <summary>
+        /// Writes a dictionary to a stream as a multipart/form-data set.
+        /// </summary>
+        /// <param name="dictionary">The dictionary of form values to write to the stream.</param>
+        /// <param name="stream">The stream to which the form data should be written.</param>
+        /// <param name="mimeBoundary">The MIME multipart form boundary string.</param>
+        /// <exception cref="System.ArgumentNullException">
+        /// Thrown if <paramref name="stream" /> or <paramref name="mimeBoundary" /> is <see langword="null" />.
+        /// </exception>
+        /// <exception cref="System.ArgumentException">
+        /// Thrown if <paramref name="mimeBoundary" /> is empty.
+        /// </exception>
+        /// <remarks>
+        /// If <paramref name="dictionary" /> is <see langword="null" /> or empty,
+        /// nothing wil be written to the stream.
+        /// </remarks>
+        public static void WriteMultipartFormData(
+          this Dictionary<string, string> dictionary,
+          Stream stream,
+          string mimeBoundary)
+        {
+            if (dictionary == null || dictionary.Count == 0)
+            {
+                return;
+            }
+            if (stream == null)
+            {
+                throw new ArgumentNullException("stream");
+            }
+            if (mimeBoundary == null)
+            {
+                throw new ArgumentNullException("mimeBoundary");
+            }
+            if (mimeBoundary.Length == 0)
+            {
+                throw new ArgumentException("MIME boundary may not be empty.", "mimeBoundary");
+            }
+            foreach (string key in dictionary.Keys)
+            {
+                string item = String.Format(FormDataTemplate, mimeBoundary, key, dictionary[key]);
+                byte[] itemBytes = System.Text.Encoding.UTF8.GetBytes(item);
+                stream.Write(itemBytes, 0, itemBytes.Length);
+            }
+        }
+    }
+
+    /// <summary>
+    /// Extension methods for <see cref="System.IO.FileInfo"/>.
+    /// </summary>
+    public static class FileInfoExtensions
+    {
+        /// <summary>
+        /// Template for a file item in multipart/form-data format.
+        /// </summary>
+        public const string HeaderTemplate = "--{0}\r\nContent-Disposition: form-data; name=\"{1}\"; filename=\"{2}\"\r\nContent-Type: {3}\r\n\r\n";
+
+
+        
+
+        /// <summary>
+        /// Writes a file to a stream in multipart/form-data format.
+        /// </summary>
+        /// <param name="file">The file that should be written.</param>
+        /// <param name="stream">The stream to which the file should be written.</param>
+        /// <param name="mimeBoundary">The MIME multipart form boundary string.</param>
+        /// <param name="mimeType">The MIME type of the file.</param>
+        /// <param name="formKey">The name of the form parameter corresponding to the file upload.</param>
+        /// <exception cref="System.ArgumentNullException">
+        /// Thrown if any parameter is <see langword="null" />.
+        /// </exception>
+        /// <exception cref="System.ArgumentException">
+        /// Thrown if <paramref name="mimeBoundary" />, <paramref name="mimeType" />,
+        /// or <paramref name="formKey" /> is empty.
+        /// </exception>
+        /// <exception cref="System.IO.FileNotFoundException">
+        /// Thrown if <paramref name="file" /> does not exist.
+        /// </exception>
+        public static void WriteMultipartFormData(
+          this FileInfo file,
+          Stream stream,
+          string mimeBoundary,
+          string mimeType,
+          string formKey)
+        {
+            if (file == null)
+            {
+                throw new ArgumentNullException("file");
+            }
+            if (!file.Exists)
+            {
+                throw new FileNotFoundException("Unable to find file to write to stream.", file.FullName);
+            }
+            if (stream == null)
+            {
+                throw new ArgumentNullException("stream");
+            }
+            if (mimeBoundary == null)
+            {
+                throw new ArgumentNullException("mimeBoundary");
+            }
+            if (mimeBoundary.Length == 0)
+            {
+                throw new ArgumentException("MIME boundary may not be empty.", "mimeBoundary");
+            }
+            if (mimeType == null)
+            {
+                throw new ArgumentNullException("mimeType");
+            }
+            if (mimeType.Length == 0)
+            {
+                throw new ArgumentException("MIME type may not be empty.", "mimeType");
+            }
+            if (formKey == null)
+            {
+                throw new ArgumentNullException("formKey");
+            }
+            if (formKey.Length == 0)
+            {
+                throw new ArgumentException("Form key may not be empty.", "formKey");
+            }
+            string header = String.Format(HeaderTemplate, mimeBoundary, formKey, file.Name, mimeType);
+            byte[] headerbytes = Encoding.UTF8.GetBytes(header);
+            stream.Write(headerbytes, 0, headerbytes.Length);
+            using (FileStream fileStream = new FileStream(file.FullName, FileMode.Open, FileAccess.Read))
+            {
+                byte[] buffer = new byte[1024];
+                int bytesRead = 0;
+                while ((bytesRead = fileStream.Read(buffer, 0, buffer.Length)) != 0)
+                {
+                    stream.Write(buffer, 0, bytesRead);
+                }
+                fileStream.Close();
+            }
+            byte[] newlineBytes = Encoding.UTF8.GetBytes("\r\n");
+            stream.Write(newlineBytes, 0, newlineBytes.Length);
+        }
+    }
+}
